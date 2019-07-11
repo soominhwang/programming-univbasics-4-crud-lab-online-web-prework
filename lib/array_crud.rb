ec7926b8af622c8007b2381f2aec166b2726b2fc
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-11 18:36:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,9 +23,9 @@ def remove_element_from_end_of_array(array)
   array.pop
 end
 
-
 def remove_element_from_start_of_array(array)
-
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
