7e21dae91a110c7f5b179db1834ced3e0b36fa48
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-11 18:33:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,8 +14,8 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  element = "wow
-  array.unshift ("")
+  element = "wow"
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
